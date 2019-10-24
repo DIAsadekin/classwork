b1133e8ad35f21c0d5884f959a19430ebf822ab4
DIAsadekin <56949277+DIAsadekin@users.noreply.github.com>
2019-10-24 11:40:51 +0600

    Add files via upload

---
+++ lesson5a.py
@@ -0,0 +1,14 @@
+#python for Loop
+#===============
+
+"""
+syntax of for Loop
+for val in sequence : 
+Body of for
+"""
+for x in [7,33,5] :
+     print (x,end="\t")
+
+for x in range (0,99):
+     print (x,end=" ")
+     print (x)
